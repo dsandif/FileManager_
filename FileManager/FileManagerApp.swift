89eac5197feaeca302ff4afb9fb5b829482a1043
Darien <dsis335@icloud.com>
2021-08-12 15:15:46 -0500

    Initial Commit

---
+++ FileManager/FileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerApp.swift
+//  FileManager
+//
+//  Created by Darien Sandifer on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
